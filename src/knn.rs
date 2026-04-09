281c991a1d73c84c13508cc8334064e54598473b
Tim Kochs <tkochs@techfak.uni-bielefeld.de>
2026-04-09 15:46:13 +0200

    added sanity check when constructing

--- src/knn.rs
+++ src/knn.rs
@@ -18,13 +18,14 @@ pub struct KnnImputer {
 }
 
 const ALLOWED_WEIGHTS: [&str; 2] = ["uniform", "distance"];
+const ALLOWED_METRICS: [&str; 2] = ["nan_euclid", "expected_distance"];
 
 #[pymethods]
 impl KnnImputer {
     #[new]
     #[pyo3(signature = (k=5, metric="nan_euclid", weights="uniform"))]
     pub fn new(k: usize, metric: &str, weights: &str) -> KnnImputer {
-        assert!(ALLOWED_WEIGHTS.contains(&weights));
+        KnnImputer::sanity_check(&metric, &weights);
         KnnImputer {
             k,
             nrows: 0,
@@ -152,6 +153,21 @@ impl KnnImputer {
             "uniform" => distances.iter().map(|_| 1.0 / self.k as f64).collect(),
             "distances" => distances.iter().map(|d| 1.0 / d).collect(),
             w => panic!("Unknown weight {}", w),
+        }
+    }
+
+    fn sanity_check(metric: &str, weights: &str) {
+        if !ALLOWED_WEIGHTS.contains(&weights) {
+            panic!(
+                "Please select a valid metric: [{:?}]\n{} is not supported",
+                ALLOWED_WEIGHTS, weights
+            );
+        }
+        if !ALLOWED_METRICS.contains(&metric) {
+            panic!(
+                "Please select a valid metric: [{:?}]\n{} is not supported",
+                ALLOWED_METRICS, metric
+            );
         }
     }
 }
