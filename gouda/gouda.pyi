a6fc311729ca332951e067ce5d8e40a0a314091a
Tim Kochs <tkochs@techfak.uni-bielefeld.de>
2026-06-11 13:27:52 +0200

    updated typing info

--- gouda/gouda.pyi
+++ gouda/gouda.pyi
@@ -1,34 +1,52 @@
-import numpy
-import numpy.typing
-from typing import Literal
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+from typing import Literal, Self
+
 
 class ConstantImputer:
-    def __init__(self, constant: float) -> None: ...
+    def __init__(self, constant: float,
+                 encoding: None | Literal["label"] = None
+                 ) -> None: ...
+
     @staticmethod
-    def zero() -> ConstantImputer:
-        ...
-    def fit(self, _data:numpy.typing.NDArray[numpy.float64]) -> ConstantImputer:
+    def zero() -> "ConstantImputer":
         ...
 
-    def transform(self, data:numpy.typing.NDArray[numpy.float64]) -> numpy.typing.NDArray[numpy.float64]:
+    def fit(self, _data: npt.NDArray[np.float64] | pd.DataFrame) -> Self:
+        ...
+
+    def transform(self, data: npt.NDArray[np.float64] | pd.DataFrame) \
+            -> npt.NDArray[np.float64]:
         ...
 
 
 class KnnImputer:
     k: int
-    def __init__(self, k:int=5, metric:str="nan_euclid", weights:str="uniform") -> None: ...
-    def fit(self, data:numpy.typing.NDArray[numpy.float64]) -> KnnImputer:
+
+    def __init__(self, k: int = 5, metric: str = "nan_euclid",
+                 weights: str = "uniform",
+                 encoding: None | Literal["label"] = None
+                 ) -> None: ...
+
+    def fit(self, data: npt.NDArray[np.float64] | pd.DataFrame) -> Self:
         ...
 
-    def transform(self, data:numpy.typing.NDArray[numpy.float64]) -> numpy.typing.NDArray[numpy.float64]:
+    def transform(self, data: npt.NDArray[np.float64] | pd.DataFrame) \
+            -> npt.NDArray[np.float64]:
         ...
 
 
 class SimpleImputer:
-    def fit(self, data:numpy.typing.NDArray[numpy.float64]) -> SimpleImputer:
+    def __init__(self,
+                 encoding: None | Literal["label"] = None
+                 ) -> None: ...
+
+    def fit(self, data: npt.NDArray[np.float64] | pd.DataFrame) -> Self:
         ...
 
-    def transform(self, data:numpy.typing.NDArray[numpy.float64]) -> numpy.typing.NDArray[numpy.float64]:
+    def transform(self, data: npt.NDArray[np.float64] | pd.DataFrame) \
+            -> npt.NDArray[np.float64]:
         ...
 
 
@@ -36,11 +54,19 @@ class Mice:
     n_iterations: int = 15
     backend: Literal["linear", "ridge", "pmm"] = "linear"
     alpha: float = 1.0
-    def __init__(self, max_iter: int = 10, backend: Literal["linear", "ridge", "pmm"]= "linear", alpha: float = 1.0, pmm_backend: Literal["linear", "ridge"] = "linear") -> None: ...
-    def fit(self, data:numpy.typing.NDArray[numpy.float64]) -> Mice:
+
+    def __init__(self, max_iter: int = 10,
+                 backend: Literal["linear", "ridge", "pmm"] = "linear",
+                 alpha: float = 1.0,
+                 pmm_backend: Literal["linear", "ridge"] = "linear",
+                 encoding: None | Literal["label"] = None
+                 ) -> None: ...
+
+    def fit(self, data: npt.NDArray[np.float64] | pd.DataFrame) -> Self:
         ...
 
-    def transform(self, data:numpy.typing.NDArray[numpy.float64]) -> numpy.typing.NDArray[numpy.float64]:
+    def transform(self, data: npt.NDArray[np.float64] | pd.DataFrame) \
+            -> npt.NDArray[np.float64]:
         ...
 
 
@@ -51,9 +77,15 @@ class MissForest:
     seed: int | None
     min_samples_leaf: int
 
-    def __init__(self, n_trees: int = 15, max_depth: int = 15, min_samples_leaf: int = 5, seed: int | None = None) -> None: ...
-    def fit(self, data:numpy.typing.NDArray[numpy.float64]) -> Mice:
+    def __init__(self, n_trees: int = 15, max_depth: int = 15,
+                 min_samples_leaf: int = 5, seed: int | None = None,
+                 encoding: None | Literal["label"] = None
+                 ) -> None: ...
+
+    def fit(self, data: npt.NDArray[np.float64] | pd.DataFrame) \
+            -> Self:
         ...
 
-    def transform(self, data:numpy.typing.NDArray[numpy.float64]) -> numpy.typing.NDArray[numpy.float64]:
+    def transform(self, data: npt.NDArray[np.float64] | pd.DataFrame) \
+            -> npt.NDArray[np.float64]:
         ...
