ef2012a016e066e7451e59fbd60ff60688bd98e0
Tim Kochs <tkochs@techfak.uni-bielefeld.de>
2026-06-18 14:54:34 +0200

    Bugfix for Mice and categorical values (#8)
    * uodated tests
    
    * bugfix categorical values mice
    
    * bump

--- tests/test_mice.py
+++ tests/test_mice.py
@@ -5,6 +5,7 @@ import pytest
 from gouda import Mice, SimpleImputer
 import time
 import pandas as pd
+# from hyperimpute.plugins.imputers import Imputers
 
 
 def test_nans():
@@ -33,8 +34,13 @@ def test_iterations_work():
     print("data:\n", data)
     print("imputed:\n", imputed)
     print("imputed2:\n", imputed2)
-    assert np.allclose(
-        imputed, imputed2), "Unexpected difference between different iterations"
+    diff = np.abs(imputed - imputed2)
+    print("diff:\n", diff)
+    print("max diff:\n", diff.max())
+    print(f"stats:\nmean: {imputed.mean()} min: {
+          imputed.min()} max: {imputed.max()}")
+    assert np.allclose(imputed, imputed2, atol=1e-3), \
+        "Unexpected difference between different iterations"
     print("imputed3:\n", imputed3)
     assert not np.allclose(
         imputed, imputed3), "No difference between iterations"
@@ -56,12 +62,20 @@ def test_time():
     data = np.random.rand(500, 12)
     data[data < 0.78] = np.nan
     max_iter = 10
+    # hyper_mice = Imputers().get("mice")
 
     N = 3
 # Warmup
     for _ in range(3):
         _ = Mice(max_iter=max_iter).fit(data).transform(data)
         _ = IterativeImputer(max_iter=max_iter).fit(data).transform(data)
+
+# # Benchmark Hyper
+#     times_h = []
+#     for _ in range(N):
+#         start = time.perf_counter_ns()
+#         _ = hyper_mice.fit_transform(data)
+#         times_h.append(time.perf_counter_ns() - start)
 
 # Benchmark Rust
     times_rs = []
@@ -81,9 +95,12 @@ def test_time():
 
     elapsed_rs = sorted(times_rs)[N // 2]  # median
     elapsed_sk = sorted(times_sk)[N // 2]
+    # elapsed_h = sorted(times_h)[N // 2]
 
     assert 4 * elapsed_rs < elapsed_sk, f"Rust: {
         elapsed_rs}ns  sklearn: {elapsed_sk}ns"
+    # assert 4 * elapsed_rs < elapsed_h, f"Rust: {
+    #     elapsed_rs}ns  hyper impute: {elapsed_h}ns"
 
 
 def test_categoricals():
@@ -97,5 +114,14 @@ def test_categoricals():
     print("data", data.iloc[:20])
     print("imputed", imputed[:20])
     assert isinstance(imputed, pd.DataFrame), "No DataFrame returned"
-    for l in imputed.iloc[mask[:, 1], 1]:
-        assert l == "a"
+    for val in imputed.iloc[mask[:, 1], 1]:
+        assert val == "a"
+
+
+def test_shape_mismatch():
+    data = pd.read_csv("tests/resources/test.csv")
+    print(data)
+    mask = np.random.rand(*data.shape) > 0.5
+    data[mask] = pd.NA
+    imputed = Mice(max_iter=2, encoding='label').fit(data).transform(data)
+    print(imputed)
